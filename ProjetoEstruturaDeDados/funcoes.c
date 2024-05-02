78a4ed825a35402e8409c6c03eb250a5ee90aa78
Mariah Gomes <141663285+Mariah-Gomes@users.noreply.github.com>
2024-05-01 23:55:06 -0300

    Update funcoes.c
    Função atendimento

--- ProjetoEstruturaDeDados/funcoes.c
+++ ProjetoEstruturaDeDados/funcoes.c
@@ -24,6 +24,13 @@ void menu_cadastrar(){
     printf("6. Voltar para o menu\n");
 }
 
+void menu_atendimento(){
+  printf("1. Enfileirar paciente\n");
+  printf("2. Desenfileirar paciente\n");
+  printf("3. Mostrar fila\n");
+  printf("4. Voltar para o menu\n");
+}
+
 void sobre(){
     printf("Desenvolvedores:\n");
     printf("Nome: Iago Rosa de Oliveira   R.A.: 24.123.056-4\n");
@@ -197,4 +204,112 @@ void cadastrar(Lista *lista){
         }
         printf("\n");
     }
+}
+
+Fila *criar_fila(){
+  Fila *f = malloc(sizeof(Fila));
+  f->head = NULL;
+  f->tail = NULL;
+  f->qtde = 0;
+  return f;
+}
+
+EFila *criar_efila(Registro dados){
+  EFila *ef = malloc(sizeof(EFila));
+  ef->anterior = NULL;
+  ef->proximo = NULL;
+  ef->dados = dados;
+  return ef;
+}
+
+void enfileirar(Lista *lista, Fila *f){
+  ELista *atual = lista->inicio;
+  char rg_desejado[10];
+  printf("Digite o numero do RG do paciente: \n");
+  scanf("%s", rg_desejado);
+  while (atual != NULL && strcmp(atual->dados.rg, rg_desejado) != 0){
+    atual = atual->proximo;
+  }
+  if(atual == NULL){
+    printf("\n");
+    printf("Paciente nao existente!");
+    printf("\n");
+    return;
+  }
+  else{
+    EFila *nova = criar_efila(atual->dados);
+    if(f->qtde == 0){
+      f->head = nova;
+    }
+    else{
+      f->tail->proximo = nova;
+      nova->anterior = f->tail;
+    }
+  f->tail = nova;
+  f->qtde++;
+  }
+}
+
+int desenfileirar(Fila *f){
+  if(f->qtde == 0){
+    return -1;
+  }
+  Registro dados = f->head->dados;
+  EFila *endereco = f->head;
+  EFila *guardar = f->head;
+  EFila *proximo = endereco->proximo;
+  if(f->qtde == 1){
+    f->tail = NULL; 
+  }
+  else{
+    proximo->anterior = NULL;
+  }
+  f->head = proximo;
+  f->qtde--;
+  free(endereco);
+  return guardar;
+}
+
+void mostrar(Fila *f){
+    EFila *atual = f->head;
+    while (atual != NULL){
+        printf("Nome: %s\n", atual->dados.nome);
+        printf("Idade: %d\n", atual->dados.idade);
+        printf("RG: %s\n", atual->dados.rg);
+        printf("Data: %d/%d/%d\n", atual->dados.entrada.dia, atual->dados.entrada.mes, atual->dados.entrada.ano);
+        printf("\n");
+        atual = atual->proximo;
+    }
+}
+
+void atendimento(Lista *lista){
+  Fila *f = criar_fila();
+  while(1){
+    int opcao_atendimento;
+    printf("Atendimento...\n");
+    menu_atendimento();
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao_atendimento);
+    printf("\n");
+    if(opcao_atendimento == 1){
+      enfileirar(lista, f);
+      printf("\n");
+    }
+    else if(opcao_atendimento == 2){
+      desenfileirar(f);
+      printf("Paciente saiu da fila\n");
+      printf("\n");
+    }
+    else if(opcao_atendimento == 3){
+      mostrar(f);
+      printf("\n");
+    }
+    else if(opcao_atendimento == 4){
+      break;
+    }
+    else{
+      printf("Opcao Invalida\n");
+      printf("\n");
+    }
+  }
 }

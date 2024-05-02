ce89b2da92fa1eb1401a84798a54dbf1f9488648
Mariah Gomes <141663285+Mariah-Gomes@users.noreply.github.com>
2024-05-01 23:56:18 -0300

    Update main.c
    Função atendimento

--- ProjetoEstruturaDeDados/main.c
+++ ProjetoEstruturaDeDados/main.c
@@ -6,31 +6,30 @@
 
 int main() {
     Lista *lista = inicializa_lista();
-
+  
     int opcao;
-
-    while(1){
-        menu();
-        printf("Selecione uma opcao: ");
-        scanf("%d", &opcao);
-        printf("\n");
-        if(opcao == 1){
-            cadastrar(lista);
-        }else if(opcao == 2){
-            printf("Atendimento...\n");
-        }else if(opcao == 3){
-            printf("Pesquisa...\n");
-        }else if(opcao == 4){
-            printf("Carregar/Salvar...\n");
-        }else if(opcao == 5){
-            sobre();
-        }else if(opcao == 6){
-            printf("Sair...\n");
-            break;
-        }else{
-            printf("Erro, escolha novamente!\n");
-        }
-        printf("\n");
+    
+    while(1){   
+      menu();
+      printf("Selecione uma opcao: ");
+      scanf("%d", &opcao);
+      printf("\n");
+      if(opcao == 1){
+        cadastrar(lista);
+      }else if(opcao == 2){
+        atendimento(lista);
+      }else if(opcao == 3){
+        printf("Pesquisa...\n");
+      }else if(opcao == 4){
+        printf("Carregar/Salvar...\n");
+      }else if(opcao == 5){
+        sobre();
+      }else if(opcao == 6){
+        printf("Sair...\n");
+        break;
+      }else{
+        printf("Erro, escolha novamente!\n");
+      }
     }
 
     return 0;
